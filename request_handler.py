674b10d724d1b136351239a75ef0e232a0b85738
MelissaCFox <melissa.caitlin.fox@gmail.com>
2022-01-13 13:20:42 -0600

    classes to find customers by name and email

--- request_handler.py
+++ request_handler.py
@@ -7,7 +7,8 @@ from views import (create_animal, create_customer, create_employee,
                    get_all_customers, get_all_employees, get_all_locations,
                    get_single_animal, get_single_customer, get_single_employee,
                    get_single_location, update_animal, update_customer,
-                   update_employee, update_location)
+                   update_employee, update_location, get_customers_by_email, 
+                   get_customers_by_name)
 
 
 # Here's a class. It inherits from another class.
@@ -20,25 +21,33 @@ class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
         path_params = path.split("/")
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
-        try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
 
-        return (resource, id)  # This is a tuple
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id)
 
 
     # Here's a class function
@@ -69,37 +78,58 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any GET request.
     def do_GET(self):
         self._set_headers(200)
-        response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        response = {}
 
-        if resource == "animals":
-            if id is not None:
-                response = f"{get_single_animal(id)}"
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
 
-            else:
-                response = f"{get_all_animals()}"
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/animals` or `/animals/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
 
-        elif resource == "locations":
-            if id is not None:
-                response = f"{get_single_location(id)}"
-            else:
-                response = f"{get_all_locations()}"
+            if resource == "animals":
+                if id is not None:
+                    response = f"{get_single_animal(id)}"
+                else:
+                    response = f"{get_all_animals()}"
+            elif resource == "customers":
+                if id is not None:
+                    response = f"{get_single_customer(id)}"
+                else:
+                    response = f"{get_all_customers()}"
+            elif resource == "locations":
+                if id is not None:
+                    response = f"{get_single_location(id)}"
+                else:
+                    response = f"{get_all_locations()}"
+            elif resource == "employees":
+                if id is not None:
+                    response = f"{get_single_employee(id)}"
+                else:
+                    response = f"{get_all_employees()}"
 
-        elif resource == "employees":
-            if id is not None:
-                response = f"{get_single_employee(id)}"
-            else:
-                response = f"{get_all_employees()}"
+        # Response from parse_url() is a tuple with 3
+        # items in it, which means the request was for
+        # `/resource?parameter=value`
+        elif len(parsed) == 3:
+            ( resource, key, value ) = parsed
 
-        elif resource == "customers":
-            if id is not None:
-                response = f"{get_single_customer(id)}"
-            else:
-                response = f"{get_all_customers()}"
+            # Is the resource `customers` and was there a
+            # query parameter that specified the customer
+            # email as a filtering value?
+            if key == "email" and resource == "customers":
+                response = get_customers_by_email(value)
+            elif key == "name" and resource == "customers":
+                first_name = value.split("+")[0]
+                last_name = value.split("+")[1]
+                full_name = f'{first_name} {last_name}'
+                response = get_customers_by_name(full_name)
 
         self.wfile.write(response.encode())
+
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
--- views/__init__.py
+++ views/__init__.py
@@ -2,7 +2,8 @@ from .animal_requests import (create_animal, delete_animal, get_all_animals,
                               get_single_animal, update_animal)
 from .customer_requests import (create_customer, delete_customer,
                                 get_all_customers, get_single_customer,
-                                update_customer)
+                                update_customer, get_customers_by_email,
+                                get_customers_by_name)
 from .employee_requests import (create_employee, delete_employee,
                                 get_all_employees, get_single_employee,
                                 update_employee)
